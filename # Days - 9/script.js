6a0b4e4d31e7a175eeb2076ecbaf1519b9283868
Kushagra-Rai-Nova <72453116+Kushagra-Rai-Nova@users.noreply.github.com>
2023-06-27 03:59:36 +0530

    Add files via upload

---
+++ # Days - 9/script.js
@@ -0,0 +1,25 @@
+const sounds = ['tone-1', 'tone-2', 'tone-3']
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
